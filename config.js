100b56167f803a1c9429a35b103de512a6ac3104
marcosbraz584 <marcosbraz88@yahoo.com.br>
2026-08-21 22:52:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -12,5 +12,5 @@
 const ACHADINHOS = {
   planilha_configuracoes: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_5XiBT9b521Uk3-H2UhfTB6Scbg7WpBRT1X5fURSL8zym27h9Z_mY8eYK9IeKl_UYSW3E1098C414/pub?gid=480609827&single=true&output=csv",
   planilha_catalogo: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_5XiBT9b521Uk3-H2UhfTB6Scbg7WpBRT1X5fURSL8zym27h9Z_mY8eYK9IeKl_UYSW3E1098C414/pub?gid=952991100&single=true&output=csv",
-  registrar_cliques: "",
+  registrar_cliques: ""
 };
